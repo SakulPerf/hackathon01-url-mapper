0c546ce21bf7b477090f03d2e8dd5400c1fe1a15
Sakul Jaruthanaset <sakul.jaruthanaset@hotmail.com>
2018-10-06 20:56:16 +0700

    Complete scenarios for segment separation.

--- UrlMapper.Tests/SimpleStringParameterTests.cs
+++ UrlMapper.Tests/SimpleStringParameterTests.cs
@@ -6,12 +6,48 @@ namespace UrlMapper.Tests
 {
     public class SimpleStringParameterTests
     {
-        [Fact]
-        public void BuilderCanCreateStringParameterObjectCorrectly()
+        private SimpleStringParameterBuilder builder;
+
+        public SimpleStringParameterTests()
+            => builder = new SimpleStringParameterBuilder();
+
+        [Theory(DisplayName = "Send any string to the builder it alway return object.")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        [InlineData("www.something.com")]
+        [InlineData("www.something.com/{username}")]
+        public void BuilderCanCreateStringParameterObjectCorrectly(string pattern)
         {
-            var sut = new SimpleStringParameterBuilder();
-            var actual = sut.Parse("www.something.com");
+            var sut = builder;
+            var actual = sut.Parse(pattern);
             actual.Should().NotBeNull();
         }
+
+        [Theory(DisplayName = "System can segment any patterns.")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        [InlineData("www.something.com", "www.something.com")]
+        [InlineData("www.something.com/sakul", "www.something.com/sakul")]
+        [InlineData("www.something.com/{username}", "www.something.com/", "{username}")]
+        [InlineData("www.something.com/{username}/service", "www.something.com/", "{username}", "/service")]
+        [InlineData("www.something.com/{username}/service/{service}", "www.something.com/", "{username}", "/service/", "{service}")]
+        [InlineData("{username}", "{username}")]
+        [InlineData("{username}something", "{username}", "something")]
+        [InlineData("www.something.com/prefix{username}", "www.something.com/prefix", "{username}")]
+        [InlineData("www.something.com/{username}postfix", "www.something.com/", "{username}", "postfix")]
+        [InlineData("{}", "{}")]
+        [InlineData("www.something.com/{}", "www.something.com/", "{}")]
+        [InlineData("{}/{}", "{}", "/", "{}")]
+        public void StringParameterCanSegmentAnyPattern(string pattern, params string[] expectedSegments)
+        {
+            var sut = GetSimpleStringParameterObj(pattern);
+            var actual = sut.SegmentPattern(pattern);
+            actual.Should().NotBeNull().And.BeEquivalentTo(expectedSegments);
+        }
+
+        private SimpleStringParameter GetSimpleStringParameterObj(string pattern)
+            => builder.Parse(pattern) as SimpleStringParameter;
     }
 }
--- UrlMapper/SimpleStringParameter.cs
+++ UrlMapper/SimpleStringParameter.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("UrlMapper.Tests")]
 namespace UrlMapper
 {
     public class SimpleStringParameter : ISimpleStringParameter
@@ -16,6 +20,43 @@ namespace UrlMapper
         public bool IsMatched(string textToCompare)
         {
             throw new System.NotImplementedException();
+        }
+
+        internal IEnumerable<string> SegmentPattern(string url)
+        {
+            var isInputValid = !string.IsNullOrEmpty(url) && !string.IsNullOrWhiteSpace(url);
+            if (!isInputValid) return Enumerable.Empty<string>();
+
+            const int MinimumRangeOfSegmentation = 0;
+
+            const string BeginSegment = "{";
+            var beginSegmentIndex = url.IndexOf(BeginSegment);
+            if (beginSegmentIndex < MinimumRangeOfSegmentation) return new[] { url };
+
+            const string EndSegment = "}";
+            var endSegmentIndex = url.IndexOf(EndSegment);
+            if (endSegmentIndex < MinimumRangeOfSegmentation) return new[] { url };
+
+            const int SeparatorOffset = 1;
+            var segments = new List<string>();
+            while (url.Contains(BeginSegment))
+            {
+                var firstSegment = url.Substring(MinimumRangeOfSegmentation, beginSegmentIndex);
+                if (!string.IsNullOrEmpty(firstSegment)) segments.Add(firstSegment);
+
+                var middleSegment = url.Substring(beginSegmentIndex, endSegmentIndex - beginSegmentIndex + SeparatorOffset);
+                segments.Add(middleSegment);
+
+                var lastSegment = url.Substring(endSegmentIndex + EndSegment.Length, url.Length - EndSegment.Length - endSegmentIndex);
+
+                url = lastSegment;
+                beginSegmentIndex = url.IndexOf(BeginSegment);
+                endSegmentIndex = url.IndexOf(EndSegment);
+
+                var shouldKeepTheLastSegment = beginSegmentIndex < MinimumRangeOfSegmentation && !string.IsNullOrEmpty(lastSegment);
+                if (shouldKeepTheLastSegment) segments.Add(lastSegment);
+            }
+            return segments;
         }
     }
 }
