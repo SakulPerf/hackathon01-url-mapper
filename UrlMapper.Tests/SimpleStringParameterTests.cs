ed26251699eee75f3fb83ca5c69cd7a67669e6f2
Sakul Jaruthanaset <sakul.jaruthanaset@hotmail.com>
2018-10-06 22:17:54 +0700

    Complete scenarios create pattern from URL.

--- UrlMapper.Tests/SimpleStringParameterTests.cs
+++ UrlMapper.Tests/SimpleStringParameterTests.cs
@@ -1,4 +1,3 @@
-using System;
 using FluentAssertions;
 using Xunit;
 
@@ -45,6 +44,41 @@ namespace UrlMapper.Tests
             var sut = GetSimpleStringParameterObj(pattern);
             var actual = sut.SegmentPattern(pattern);
             actual.Should().NotBeNull().And.BeEquivalentTo(expectedSegments);
+        }
+
+        [Theory(DisplayName = "System can create pattern from segmentation.")]
+        [InlineData("", "", "")]
+        [InlineData("", " ", "")]
+        [InlineData("", null, "")]
+        [InlineData(" ", "", "")]
+        [InlineData(" ", " ", "")]
+        [InlineData(" ", null, "")]
+        [InlineData(null, "", "")]
+        [InlineData(null, " ", "")]
+        [InlineData(null, null, "")]
+        [InlineData("www.something.com", "www.something.com", "www.something.com")]
+        [InlineData("www.something.com/sakul", "www.something.com/sakul", "www.something.com/sakul")]
+        [InlineData("www.something.com/{username}", "www.something.com/sakul", "www.something.com/{username}")]
+        [InlineData("www.something.com/{username}/service", "www.something.com/sakul/service", "www.something.com/{username}/service")]
+        [InlineData("www.something.com/{username}/service/{service}", "www.something.com/sakul/service/1234", "www.something.com/{username}/service/{service}")]
+        [InlineData("{username}", "sakul-jaruthanaset", "{username}")]
+        [InlineData("{username}something", "sakulsomething", "{username}something")]
+        [InlineData("www.something.com/prefix{username}", "www.something.com/prefixsakul", "www.something.com/prefix{username}")]
+        [InlineData("www.something.com/{username}postfix", "www.something.com/sakulpostfix", "www.something.com/{username}postfix")]
+        [InlineData("{}", "", "{}")]
+        [InlineData("{}", "sakul", "{}")]
+        [InlineData("www.something.com/{}", "www.something.com/sakul", "www.something.com/{}")]
+        [InlineData("www.something.com/{}", "www.something.com/sakul/something", "www.something.com/{}")]
+        [InlineData("{}/{}", "sakul/jaruthanaset", "{}/{}")]
+        [InlineData("{}/{}", "sakul/jaruthanaset/123/456", "{}/{}")]
+        public void StringParameterCanCreatePatternFromSegmentation(string pattern, string url, string expectedPattern)
+        {
+            var sut = GetSimpleStringParameterObj(pattern);
+            var segments = sut.SegmentPattern(pattern);
+            segments.Should().NotBeNull();
+            var actual = sut.getPattern(url, segments);
+            actual.Should().NotBeNull();
+            actual.Should().Be(expectedPattern);
         }
 
         private SimpleStringParameter GetSimpleStringParameterObj(string pattern)
--- UrlMapper/SimpleStringParameter.cs
+++ UrlMapper/SimpleStringParameter.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 [assembly: InternalsVisibleTo("UrlMapper.Tests")]
 namespace UrlMapper
@@ -57,6 +57,47 @@ namespace UrlMapper
                 if (shouldKeepTheLastSegment) segments.Add(lastSegment);
             }
             return segments;
+        }
+
+        internal string getPattern(string url, IEnumerable<string> segments)
+        {
+            var isArgumentsValid = url != null
+                && segments != null
+                && !string.IsNullOrEmpty(this.pattern);
+            if (!isArgumentsValid) return string.Empty;
+
+            const string BeginSegment = "{";
+            const string EndSegment = "}";
+            const int MinimumRangeOfSegmentation = 0;
+            var pattern = new StringBuilder();
+
+            var variableSegment = string.Empty;
+            foreach (var item in segments)
+            {
+                var beginSegmentIndex = url.IndexOf(item);
+                var isContainSegment = beginSegmentIndex >= MinimumRangeOfSegmentation;
+                var isVariableSection = item.StartsWith(BeginSegment) && item.EndsWith(EndSegment);
+                var shouldKeetpThisSegment = isContainSegment || isVariableSection;
+                if (!shouldKeetpThisSegment) continue;
+
+                if (isVariableSection)
+                {
+                    variableSegment = item;
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(variableSegment))
+                    {
+                        url = url.Remove(0, beginSegmentIndex);
+                        variableSegment = string.Empty;
+                        beginSegmentIndex = url.IndexOf(item);
+                    }
+                    url = url.Remove(beginSegmentIndex, item.Length);
+                }
+
+                pattern.Append(item);
+            }
+            return pattern.ToString();
         }
     }
 }
